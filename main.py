b46c702dee3eb73a2cbe63ffbd1c402f8b75efb2
LGE\changjo.yu <tksqhddnfl5@snu.ac.kr>
2024-02-07 14:29:33 +0900

    Add icon.png file and push button with handler function in main.py

--- main.py
+++ main.py
@@ -1,21 +1,36 @@
-
 import sys
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QWidget # 전자는 애플리케이션 핸들러, 후자는 빈 GUI 위젯
+from PyQt5.QtWidgets import (QApplication,QWidget,QPushButton,QVBoxLayout,
+                             QMessageBox)
+from PyQt5.QtGui import QIcon
+
 
 class Calculator(QWidget):
-
-    def __init__(self) :
-        super().__init__() # QWidget껄 상속받겠다.
-        self.initUI() # 나머지는 initUI()에서 정의하겠다.
-
+    
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        
     def initUI(self):
-        self.setWindowTitle('Calculator') # 윈도우에 표시되는 타이틀 이름을 칼큘레이터로 하겠다.
-        self.resize(256, 256) # 크기
+        self.btn1=QPushButton('Message',self)
+        self.btn1.clicked.connect(self.activateMessage)
+        
+        vbox=QVBoxLayout()
+        vbox.addStretch(1)
+        vbox.addWidget(self.btn1)
+        vbox.addStretch(1)
+        
+        self.setLayout(vbox)
+        
+        self.setWindowTitle('Calculator')
+        self.setWindowIcon(QIcon('icon.png'))
+        self.resize(256,256)
         self.show()
-
-if __name__ == '__main__' : # pyqt는 앱당 1개의 QApplication이 필요.
-    app = QApplication(sys.argv) # 인스턴스 생성
-    view = Calculator() # 윈도 인스턴스 생성
-    sys.exit(app.exec_()) # 애플리케이션이 이벤트 처리를 하도록 루프 구동
-
+        
+    def activateMessage(self):
+        QMessageBox.information(self,"information","Button clicked!")
+        
+        
+if __name__ == '__main__':
+    app=QApplication(sys.argv)
+    view=Calculator()
+    sys.exit(app.exec_())
